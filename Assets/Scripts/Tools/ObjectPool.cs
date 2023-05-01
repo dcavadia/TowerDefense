18b915f4fb9e604a8e64befe27df25c8c5b22078
dcavadia <dan.acppp@gmail.com>
2023-05-01 11:50:52 -0400

    add documentation to objectpool

--- Assets/Scripts/Tools/ObjectPool.cs
+++ Assets/Scripts/Tools/ObjectPool.cs
@@ -8,6 +8,9 @@ using UnityEngine;
 //Uses reflection to dynamically find all the classes derived from the generic type T
 public class ObjectPool<T> where T : Component
 {
+    //Using a queue data structure ensures that objects that have been in the pool the longest are returned first, which helps to avoid situations where objects
+    //are kept in the pool for a long time without being reused. In addition the operations of enqueueing and dequeuing items in a queue are O(1) time complexity,
+    //which makes it an efficient data structure for this purpose.
     private Queue<T> pool = new Queue<T>();
 
     public T GetObjectFromPool()
